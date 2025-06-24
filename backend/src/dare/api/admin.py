cb7c7a38957e341b9fa2adbe55a6dea989d04072
romakot321 <play62342@gmail.com>
2025-06-24 03:53:47 +0000

    Fix

--- backend/src/dare/api/admin.py
+++ backend/src/dare/api/admin.py
@@ -7,5 +7,5 @@ class DareAdmin(ModelView, model=DareDB):
     name = "Dare"
     page_size = 50
     column_list = [DareDB.language, DareDB.text, DareDB.likes, DareDB.dislikes]
-    column_sortable_list = ["language", "likes", "dislikes"]
+    column_sortable_list = ["likes", "dislikes"]
     form_excluded_columns = [DareDB.dislikes, DareDB.likes]
